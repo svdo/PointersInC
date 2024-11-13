8fa7a0f6c2707c7e5b4f624fa8c008d551833ac6
Stefan van den Oord <stefan@medicinemen.eu>
2024-11-13 10:13:49 +0100

    Mutation

--- 2-all-just-bytes.c
+++ 2-all-just-bytes.c
@@ -1,13 +1,26 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(int argc, char *argv[]) {
-  char sample[] = "ABC abc";
-  printf("String: %s\n", sample);
-  printf("Length: %lu\n", strlen(sample));
+void showStringInfo(char str[])
+{
+  printf("String: %s\n", str);
+  printf("Length: %lu\n", strlen(str));
   printf("Characters:\n");
-  for (int i = 0; i < strlen(sample); ++i) { // change < to <=
-    char c = sample[i];
+  for (int i = 0; i < strlen(str); ++i) // change < to <=
+  {
+    char c = str[i];
     printf("  %i: %c (%d / 0x%x)\n", i, c, c, c);
   }
+  printf("\n");
+}
+
+int main(int argc, char *argv[])
+{
+  char sample[] = "ABC abc";
+  showStringInfo(sample);
+
+  for (int i = 0; i < strlen(sample); ++i) {
+    sample[i] += 1;
+  }
+  showStringInfo(sample);
 }
