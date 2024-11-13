46ea3d50ab39155c08804821b3a18c7700f432d4
Stefan van den Oord <stefan@medicinemen.eu>
2024-11-13 10:01:23 +0100

    argc/argv

--- 1-hello-world.c
+++ 1-hello-world.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-    printf("Hello World!\n");
+    printf("Hello World!\n\n");
+
+    printf("Number of command line arguments: %d\n", argc);
+    for (int i = 0; i < argc; ++i) {
+        printf("    arg %d: %s\n", i, argv[i]);
+    }
+
+    printf("\nBye!\n\n");
 }
